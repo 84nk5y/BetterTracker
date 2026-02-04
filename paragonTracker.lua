4e3d59397365fb7e013336caf2151eab8e9ce39c
Banksy <445668+84nk5y@users.noreply.github.com>
2026-02-04 10:02:16 +0100

    Reworked paragon tracker

--- paragonTracker.lua
+++ paragonTracker.lua
@@ -1,4 +1,70 @@
-local SCAN_IN_PROGRESS = false
+local PARAGON_FACTIONS ={
+    -- The War Within
+    [2590] = "Council of Dornogal",
+    [2688] = "Flame's Radiance",
+    [2570] = "Hallowfall Arathi",
+    [2594] = "The Assembly of the Deeps",
+    [2658] = "The K'aresh Trust",
+    [2653] = "The Cartels of Undermine",
+        [2685] = "Gallagio Loyalty Rewards Club",
+        [2673] = "Bilgewater Cartel",
+        [2675] = "Blackwater Cartel",
+        [2669] = "Darkfuse Solutions",
+        [2677] = "Steamwheedle Cartel",
+        [2671] = "Venture Company",
+    [2600] = "The Severed Threads",
+        [2605] = "The General",
+        [2607] = "The Vizier",
+        [2601] = "The Weaver",
+
+    -- Dragonflight
+    [2574] = "Dream Wardens",
+    [2511] = "Iskaara Tuskarr",
+    [2564] = "Loamm Niffen",
+    [2503] = "Maruuk Centaur",
+    [2507] = "Dragonscale Expedition",
+    [2510] = "Valdrakken Accord",
+
+    -- Shadowlands
+    [2413] = "Court of Harvesters",
+    [2470] = "Deaths Advance",
+    [2472] = "The Archivists Codex",
+    [2407] = "The Ascended",
+    [2478] = "The Enlightened",
+    [2410] = "The Undying Army",
+    [2465] = "The Wild Hunt",
+    [2432] = "Ve,nari",
+
+    -- Battle for Azeroth
+    [2164] = "Champions of Azeroth",
+    [2415] = "Rajani",
+    [2391] = "Rustbolt Resistance",
+    [2156] = "Talanji's Expedition",
+    [2157] = "The Honorbound",
+    [2373] = "The Unshackled",
+    [2163] = "Tortollan Seekers",
+    [2417] = "Uldum Accord",
+    [2158] = "Voldunai",
+    [2103] = "Zandalari Empire",
+    [2160] = "Proudmoore Admiralty",
+    [2161] = "Order of Embers",
+    [2162] = "Storm's Wake",
+    [2159] = "7th Legion",
+
+    -- Legion
+    [2170] = "Argussian Reach",
+    [2045] = "Armies of Legionfall",
+    [2165] = "Army of the Light",
+    [1900] = "Court of Farondis",
+    [1883] = "Dreamweavers",
+    [1828] = "Highmountain Tribe",
+    [1859] = "The Nightfallen",
+    [1894] = "The Wardens",
+    [1948] = "Valarjar",
+}
+
+
+local SCAN_PENDING = false
 local AVAILABLE_PARAGON_CACHES = {}
 
 
@@ -46,47 +112,31 @@ local function UpdateParagonBadge()
 end
 
 local function GetAvailableParagonCaches()
-    if SCAN_IN_PROGRESS then return end
-
-    SCAN_IN_PROGRESS = true
     AVAILABLE_PARAGON_CACHES = {}
 
     local factionQueue = {}
-    for i = 1, C_Reputation.GetNumFactions() do
-        local _, _, _, _, _, _, _, _, isHeader, _, hasRep, factionID = C_CreatureInfo.GetFactionInfo(i)
-        if factionID and (not isHeader or hasRep) then
-            table.insert(factionQueue, { id = factionID, isMajor = false })
-        end
+
+    for factionID, name in pairs(PARAGON_FACTIONS) do
+        table.insert(factionQueue, { id = factionID, name = name })
     end
 
-    local majorFactionIDs = C_MajorFactions.GetMajorFactionIDs()
-    for _, factionID in ipairs(majorFactionIDs) do
-        table.insert(factionQueue, { id = factionID, isMajor = true })
-    end
-
-    local count = 0
     local function ScanParagonBatch(startIndex)
-        local batchSize = 2 -- Number of factions per frame
+        local batchSize = 6 -- Number of factions per frame
         local endIndex = math.min(startIndex + batchSize - 1, #factionQueue)
 
         for i = startIndex, endIndex do
             local item = factionQueue[i]
             local factionID = item.id
+            local factionName = item.name
 
-            if C_Reputation.IsFactionParagon(factionID) then
+            if C_Reputation.IsFactionParagonForCurrentPlayer(factionID) then
                 local _, _, rewardQuestID, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
 
                 if hasRewardPending then
-                    local name = ""
-                    if item.isMajor then
-                        local data = C_MajorFactions.GetMajorFactionData(factionID)
-                        name = data and data.name or "Unknown Major Faction"
-                    else
-                        name = GetFactionInfoByID(factionID) or "Unknown Faction"
-                    end
+                    print("    Faction has reward pending: "..factionName)
 
                     AVAILABLE_PARAGON_CACHES[factionID] = {
-                        name = name,
+                        name = factionName,
                         questID = rewardQuestID
                     }
                 end
@@ -98,7 +148,7 @@ local function GetAvailableParagonCaches()
         else
             UpdateParagonBadge()
 
-            SCAN_IN_PROGRESS = false
+            SCAN_PENDING = false
         end
     end
 
@@ -126,9 +176,14 @@ end
 
 f:RegisterEvent("QUEST_TURNED_IN")
 f:RegisterEvent("UPDATE_FACTION")
+f:RegisterEvent("FACTION_STANDING_CHANGED")
 f:SetScript("OnEvent", function(self, event, ...)
-    if event == "UPDATE_FACTION" then
-        GetAvailableParagonCaches()
+    if event == "UPDATE_FACTION" or "FACTION_STANDING_CHANGED" then
+        if not SCAN_PENDING then
+            SCAN_PENDING = true
+
+            C_Timer.After(0.1, GetAvailableParagonCaches)
+        end
     elseif event == "QUEST_TURNED_IN" then
         local questID = ...
 
